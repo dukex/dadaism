323b925570724bb479b100361884bc038f7194c9
Duke <duke@riseup.net>
2012-08-17 02:01:47 -0300

    clean app.rb and using DadismGenerator

--- app.rb
+++ app.rb
@@ -4,77 +4,17 @@ require 'bundler'
 Bundler.require
 $:.unshift(File.expand_path(File.join(File.dirname(__FILE__), "lib")))
 
-
-class String
-  def remove_start_space
-    self.gsub(/\r|\t|^\w/, "")
-  end
-
-  def clean_word
-    self.remove_pontaction
-  end
-
-  def remove_pontaction
-    self.gsub(/\.|,|\$|:|\(|\)|\"|“/, "")
-  end
-end
-
-
-#html = open("http://g1.globo.com/natureza/noticia/2012/08/brasil-retrocede-em-leis-federais-de-protecao-amazonia.html").read
-#parser = Nokogiri::HTML(html);
-#words = parser.search(".entry-content p").text.split("\n").map(&:remove_start_space).join("\n").split(" ")#.map(&:clean_word)
-
-#binding.pry
-#.delete_if{|w| (w.size < 3 && w.size != /a|e|i|o|u/) or w.match(/^\d+$/) }.flatten
-
-#puts words
-=begin
-word_count = 0
-verso_count = 0
-estrofe = 0
-
-
-max_verso = 4
-max_word = 4
-
-
-
-words.shuffle.each_with_index do |word, i|
-  next if word == words[i-1]
-  next if (max_word == (word_count+1) or  word_count == 0) and word.size < 3
-  word_count = word_count+1
-  word = word.gsub(".", "").gsub(/,$/, "").gsub(/ +/, "")
-
-  extra = " "
-
-  if word_count == max_word
-    extra = "\n"
-    word_count = 0
-    verso_count = verso_count+1
-    if verso_count == max_verso
-      extra = "\n\n"
-      verso_count = 0
-      estrofe = estrofe+1
-    end
-  end
-
-  print word.downcase + extra
-
-  exit if estrofe == max_verso
-end
-=end
-class Dadaism
-  TYPES = [ "Monóstico", "Dístico", "Terceto", "Quarteto ou quadra", "Quintilha", "Sextilha", "Septilha", "Oitava", "Nona", "Décima"]
-end
+require 'dadaism'
 
 class DadaismApp < Sinatra::Base
-
   get "/" do
-    @types = Dadaism::TYPES
+    @types = DadaismGenerator::TYPES
     haml :index
   end
 
   post "/poem" do
-    poem = params[:poem]
+    poem_params = params[:poem]
+    @poem = DadaismGenerator.new poem_params
+    haml :poem
   end
 end
